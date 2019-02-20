ed2e501a01cce42cd16c622ac09e7bd783875111
Olimpio <olymangue@gmail.com>
2019-02-20 19:06:39 +0200

    histogram

---
+++ histogram.rb
@@ -0,0 +1,18 @@
+puts "Enter a phrase you'd like to analyze: "
+text = gets.chomp
+
+words = text.split
+
+frequencies = Hash.new(0)
+
+words.each do |word| 
+  frequencies[word]  += 1
+end
+frequencies = frequencies.sort_by do 
+  |word, freq|
+  	freq
+end
+frequencies.reverse!
+frequencies.each do |word, freq|
+  puts word + " " + freq.to_s
+end
