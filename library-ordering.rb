0fe9ce9cd5aadbf4ffe48b4a69080593d36eb7cd
Olimpio <olymangue@gmail.com>
2019-02-22 09:56:44 -0800

    library-ordering

---
+++ library-ordering.rb
@@ -0,0 +1,13 @@
+# TDOD - comment the code
+def alphabetize(arr, rev = false)
+  arr.sort!
+  if rev == true
+    arr.reverse!
+  else
+  	arr
+  end
+end
+
+numbers = [3, 5, 1, 6]
+
+puts alphabetize(numbers, true)
